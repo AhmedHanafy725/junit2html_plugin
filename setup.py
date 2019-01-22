69424758a9c8c54c1ff3671d14cd125e7b98036b
AhmedHanafy725 <ahmed.hanafy725@gmail.com>
2019-01-22 15:16:57 +0200

    edit setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description='Generate html file from junit reports',
     author='Ahmed El-Sayed',
     author_email='ahmed.m.elsayed93@gmail.com',
-    url='http://github.com/ahmedelsayed-93/junit2html',
+    url='https://github.com/AhmedHanafy725/junit2html_plugin',
     install_requires=['jinja2'],
     # packages=['junit2html'],
     package_data = {'junit2html': ['template.html']},
